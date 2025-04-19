bbc14fce7d30c6d391314c4b8f175e4e8cf10fd7
Ryan <ryan.kiprop@student.moringaschool.com>
2025-04-19 18:15:34 +0300

    heheh react jamani

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,36 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const[tasks, setTasks]=useState(TASKS)
+  const[selectedCategory, setSelectedCategory]=useState("All")
+
+  const filteredTasks = selectedCategory === "All"
+  ? tasks
+  : tasks.filter(task => task.category === selectedCategory);
+
+function handleAddTask(newTask) {
+  setTasks([...tasks, newTask]);
+}
+
+function handleDeleteTask(taskText) {
+  setTasks(tasks.filter(task => task.text !== taskText));
+}
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter 
+      categories={CATEGORIES}
+      selectedCategory={selectedCategory}
+      onCategoryChange={setSelectedCategory}
+      />
+      <NewTaskForm 
+      categories={CATEGORIES}
+      onTaskFormSubmit={handleAddTask}
+      />
+      <TaskList 
+      tasks={filteredTasks}
+      onDeleteTask={handleDeleteTask}
+      />
     </div>
   );
 }
